18c09cd02cdf257c993e6502afe45b1389fbf981
AMEER <ameerkareem61@gmail.com>
2022-09-20 20:10:00 +0300

    v1.0.3

--- Android App/BloodFlow/app/src/main/java/com/example/bloodflow/utils/RandomUtils.java
+++ Android App/BloodFlow/app/src/main/java/com/example/bloodflow/utils/RandomUtils.java
@@ -8,7 +8,7 @@ public class RandomUtils {
 
     public static final UUID M_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     public static final String BOT_URL = "https://api.telegram.org/bot";
-    public static final String TOKEN = "5209962730:AAH4gPaaCQ964ekyAwcTGbDMjKyIOwHfzB0";
+    public static final String TOKEN = "5209962730:AAHVX-HxZIPhCxOgsaK9AJAsnU5phrF9hQs";
     public static final String AVERAGE = "Average: ";
     public static final String KEY = "KEY";
     public static final String EMPTY = "empty";
